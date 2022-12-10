3fa47f2e809e2520d777feeee81fb5576355ad84
Damian Kluczyński <damiankluczynski00@gmail.com>
2022-12-10 14:12:44 +0100

    Changed proxy, subcject and flyweight

--- main.py
+++ main.py
@@ -1,84 +1,82 @@
+import json
+from typing import Dict
 from abc import ABC, abstractmethod
-
-class SubjectInterface(ABC):
-    @abstractmethod
-    def getSubjectInfo(self):
-        pass
 
 class IFlyweight(ABC):
     @abstractmethod
     def getState(self):
         pass
 
-class RealSubject(SubjectInterface):
-    def __init__(self, first_name, last_name, latitude, longitude, flyweightFactory):
-        self._first_name = first_name
-        self._last_name = last_name
-        self._latitude = latitude
-        self._longitude = longitude
-        self._unique_state = [self._last_name, self._latitude, self._longitude]
-        self._non_unique_state = flyweightFactory.getFlyweight(self._first_name)
-
-    def getSubjectInfo(self):
-        return(self._first_name, self._last_name, self._latitude, self._longitude)
-
-class Proxy(SubjectInterface):
-    def __init__(self, first_name, last_name, latitude, longitude, flyweightFactory):
-        self._realSubject = RealSubject(first_name.lower().capitalize().replace(' ',''), last_name.lower().capitalize().replace(' ',''), latitude, longitude, flyweightFactory)
-    def getSubjectInfo(self):
-        return self._realSubject.getSubjectInfo()
-
-class Officer:
-    def __init__(self):
-        self._immigrant_first_name = ""
-        self._immigrant_last_name = ""
-        self._latitude = ""
-        self._longitude = ""
-    def setImmigrantData(self):
-        self._immigrant_first_name = str(input("Imie: "))
-        self._immigrant_last_name = str(input("Nazwisko: "))
-        self._latitude = str(input("Szerokosc geograficzna: "))
-        self._longitude = str(input("Wysokosc geograficzna: "))
-
-    def getImmigrantData(self):
-        return(self._immigrant_first_name, self._immigrant_last_name, self._latitude, self._longitude)
-
 class Flyweight(IFlyweight):
-    def __init__(self, state):
-        self._non_unique_state = state
+    def __init__(self, shared_state: str) -> None:
+        self._shared_state = shared_state
 
-    def getState(self):
-        return self._non_unique_state
+    def getState(self) -> str:
+        return self._shared_state
 
 class FlyweightFactory:
-    _flyweights = {}
 
-    def getFlyweight(self, non_unique_state):
-        if self._flyweights.get(non_unique_state) is not None:
-            print("Pobieram z cache")
-            print("typo: ", self._flyweights.get(non_unique_state))
-            return self._flyweights.get(non_unique_state)
+    _flyweights: Dict[str, Flyweight] = {}
+    def __init__(self, initial_flyweights: Dict) -> None:
+        for state in initial_flyweights:
+            self._flyweights[self.getKey(state)] = Flyweight(state)
+
+    def getKey(self, state: Dict) -> str:
+        return "_".join(sorted(state))
+
+    def getFlyweight(self, shared_state: Dict) -> Flyweight:
+        key = self.getKey(shared_state)
+        if not self._flyweights.get(key):
+            print("Creating new flyweight")
+            self._flyweights[key] = Flyweight(shared_state)
         else:
-            print("Przed dodaniem: ", self._flyweights)
-            self._flyweights[non_unique_state] = Flyweight(non_unique_state)
-            print("Po dodaniu: ", self._flyweights)
-            print("Tworze nowego typa w cachu")
+            print("Using existing flyweight")
+
+        return self._flyweights[key]
+
+def add_immigrant_to_database(factory: FlyweightFactory, first_name: str, last_name: str, latitude: str, longitude: str) -> None:
+    real_subject = RealSubject()
+    flyweight = factory.get_flyweight([first_name])
+    flyweight.operaion([last_name, latitude, longitude])
 
 
-        return self._flyweights.get(non_unique_state)
+class Subject(ABC):
+    @abstractmethod
+    def request(self) -> None:
+        pass
 
-    def getFlyweightFactory(self):
-        return self._flyweights
+class RealSubject(Subject):
+    def __init__(self) -> None:
+        self._first_name = ""
+        self._last_name = ""
+        self._latitude = ""
+        self._longitude = ""
 
-class SaveData:
-    def toFile(data):
-        with open(".\\Immigrant_data.txt", "a") as f:
-            f.writelines(str(data)+"\n")
+    def setData(self, proxy):
 
-if __name__ == '__main__':
-    while(True):
-        cache = FlyweightFactory()
-        officer = Officer()
-        officer.setImmigrantData()
-        immigrant_data = Proxy(officer._immigrant_first_name, officer._immigrant_last_name, officer._latitude, officer._longitude, cache)
-        SaveData.toFile(immigrant_data.getSubjectInfo())
+    def getNonUniqueState(self) -> str:
+        return self._first_name
+    def getUniqueState(self) -> str:
+        return [self._last_name, self._latitude, self._latitude]
+
+class Proxy(Subject):
+    def __init__(self, first_name: str,last_name: str, latitude: str, longitude: str) -> None:
+        self._first_name = first_name.lower().capitalize()
+        self._last_name = last_name.lower().capitalize()
+        self._latitude = latitude
+        self._longitude = longitude
+        self._real_subject = RealSubject()
+
+    def checkData(self) -> None:
+        if self.check_access():
+            self._real_subject.request()
+            self.log_access()
+
+
+def client_code(subject: Subject) -> None:
+    subject.request()
+
+if __name__ == "__main__":
+    real_subject = RealSubject()
+    proxy = Proxy(real_subject)
+    client_code(proxy)
